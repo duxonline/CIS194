0da9a065f45f8e62414dc392124685f86eeb1abe
Frank Wang <FrankWangSydney@gmail.com>
2020-01-19 17:10:02 +1100

    Added the solution for week 1

---
+++ src/Week1.hs
@@ -0,0 +1,27 @@
+module Week1 where
+
+toDigits :: Int -> [Int]
+toDigits n
+    | n > 0 = toDigits (n `div` 10) ++ [n `mod` 10]
+    | otherwise = []
+
+toDigitsRev :: Int -> [Int]
+toDigitsRev n
+    | n > 0 = [n `mod` 10] ++ toDigitsRev(n `div` 10)
+    | otherwise = []
+
+doubleEveryOther :: [Int] -> [Int]
+doubleEveryOther l = reverse $ zipWith (*) (reverse l) $ cycle [1,2]
+
+sumDigits :: [Int] -> [Int]
+sumDigits = concatMap toDigits
+
+validate :: Int -> Bool
+validate n = 
+    case (sum . sumDigits . doubleEveryOther . toDigits) n `mod` 10 of
+        0 -> True 
+        _ -> False 
+
+reverse2 :: [Int] -> [Int]
+reverse2 [] = []
+reverse2 (x:zs) = reverse2 zs ++ [x]
