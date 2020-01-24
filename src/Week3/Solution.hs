07ee247bfd800677ce83c84e31ff0eded1584dc0
Frank Wang <FrankWangSydney@gmail.com>
2020-01-25 08:56:01 +1100

    Added localMaxima function

--- src/Week3/Solution.hs
+++ src/Week3/Solution.hs
@@ -5,3 +5,9 @@ skips list = [each i list | i <- [1..length list -1]]
 
 each :: Int -> [a] -> [a]
 each n list = [ list !! i | i <- [n-1, n-1+n..length list -1]]
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima (x:y:z:zx)
+    | x < y && y > z = y : localMaxima (y:z:zx)
+    | otherwise = localMaxima (y:z:zx)
+localMaxima _ = []
