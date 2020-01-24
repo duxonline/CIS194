c1809b90a8362d75e3fac075c92046d90d6dd6f1
Frank Wang <FrankWangSydney@gmail.com>
2020-01-25 07:55:59 +1100

    Finished skips function

--- src/Week3/Solution.hs
+++ src/Week3/Solution.hs
@@ -1,1 +1,7 @@
 module Week3.Solution where
+
+skips :: [a] -> [[a]]    
+skips list = [each i list | i <- [1..length list -1]]
+
+each :: Int -> [a] -> [a]
+each n list = [ list !! i | i <- [n-1, n-1+n..length list -1]]
