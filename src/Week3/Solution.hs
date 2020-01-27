d1c0cc60624346936ff9faa714986202d6c8e031
Frank Wang <FrankWangSydney@gmail.com>
2020-01-27 22:05:40 +1100

    Added createLine function

--- src/Week3/Solution.hs
+++ src/Week3/Solution.hs
@@ -6,12 +6,14 @@ skips lst = [each i lst | i <- [1..length lst -1]]
 each :: Int -> [a] -> [a]
 each n lst = [ lst !! i | i <- [n-1, n-1+n..length lst -1]]
 
-localMaxima :: [Integer] -> [Integer]
+localMaxima :: [Int] -> [Int]
 localMaxima (x:y:z:zx)
     | x < y && y > z = y : localMaxima (y:z:zx)
     | otherwise = localMaxima (y:z:zx)
 localMaxima _ = []
 
+createLine :: [Int] -> Int -> String
+createLine lst n = [if x >= n then '*' else ' ' | x <- lst]
 
 frequency :: [Int] -> [Int]
 frequency lst = [length (filter (==x) lst) | x <- [0..9]]
