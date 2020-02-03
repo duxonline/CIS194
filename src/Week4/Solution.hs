cdd34e0d406a0d577e653590952547bfc03d2099
Frank Wang <FrankWangSydney@gmail.com>
2020-02-03 15:17:05 +1100

    Added fun2

--- src/Week4/Solution.hs
+++ src/Week4/Solution.hs
@@ -8,3 +8,15 @@ module Week4.Solution where
 
 fun1 :: [Integer] -> Integer
 fun1 = product . filter even
+
+-- fun2 :: Integer -> Integer
+-- fun2 1 = 0
+-- fun2 n 
+--     | even n = n + fun2 (n `div` 2)
+--     | otherwise = fun2 (3 * n + 1)
+
+fun2 :: Integer -> Integer
+fun2 = sum
+      .filter even
+      .takeWhile (/=1)
+      .iterate (\n -> if even n then n `div` 2 else 3 * n +1)
