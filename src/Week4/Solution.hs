b8946d2cb8a4e5a5465e07bf913b4c62c1c17313
Frank Wang <FrankWangSydney@gmail.com>
2020-02-04 07:30:32 +1100

    Fixed the bug in fun1

--- src/Week4/Solution.hs
+++ src/Week4/Solution.hs
@@ -9,7 +9,7 @@ import Data.List
 --     | otherwise = fun1 xs
 
 fun1 :: [Integer] -> Integer
-fun1 = product . filter even
+fun1 = product . map (subtract 2) . filter even
 
 -- fun2 :: Integer -> Integer
 -- fun2 1 = 0
