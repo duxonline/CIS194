c73c2acd42290c26f3f041e29bb96e6a18b043db
Frank Wang <FrankWangSydney@gmail.com>
2020-02-05 17:00:38 +1100

    Added the instance of type class Expr

--- src/Week5/Solution.hs
+++ src/Week5/Solution.hs
@@ -12,3 +12,13 @@ evalStr :: String -> Maybe Integer
 evalStr s = case parseExp Lit Add Mul s of
             Nothing -> Nothing 
             Just p -> Just $ eval p
+
+class Expr a where 
+    lit :: Integer -> a
+    add :: a -> a -> a
+    mul :: a -> a -> a
+
+instance Expr ExprT where
+    lit = Lit
+    add = Add
+    mul = Mul
