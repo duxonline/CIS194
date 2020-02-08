33e3cf64b0c711f526f6b86ad4c31fde08f777bb
Frank Wang <FrankWangSydney@gmail.com>
2020-02-08 13:55:46 +1100

    Added performant fibs2

--- src/Week6/Solution.hs
+++ src/Week6/Solution.hs
@@ -7,3 +7,9 @@ fib n = fib (n-1) + fib (n-2)
 
 fibs1 :: [Integer]
 fibs1 = map fib [0..] 
+
+fibs2 :: [Integer]
+fibs2 = f 0 1
+    where f a b = a : f b (a + b)
+
+-----------------------------------------------------------
