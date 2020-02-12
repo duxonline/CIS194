5864a34a32a0b88b42acf69a82f446691e1e4d4f
Frank Wang <FrankWangSydney@gmail.com>
2020-02-12 22:47:40 +1100

    Added dropJ function

--- src/Week7/Solution.hs
+++ src/Week7/Solution.hs
@@ -26,10 +26,10 @@ someJoinList =
     (Single (L.Product 7) 'h')
 
 (!!?) :: [a] -> Int -> Maybe a
-[]      !!? _ =          Nothing
-_       !!? n | n < 0 =  Nothing 
-(x:xs)  !!? 0 =          Just x
-(x:xs)  !!? n =          xs !!? (n-1)
+[]      !!? _         =  Nothing
+_       !!? n | n < 0 =  Nothing
+(x:xs)  !!? 0         =  Just x
+(x:xs)  !!? n         =  xs !!? (n-1)
 
 jlToList :: JoinList m a -> [a]
 jlToList Empty =        []
@@ -38,6 +38,26 @@ jlToList (Append _ l r) = jlToList l ++ jlToList r
 
 indexJ :: (S.Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
 indexJ n lst = jlToList lst !!? n
+
+dropJ :: (S.Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ n jl@(Single _ _)
+  | n <= 0 = jl
+dropJ n jl@(Append m jl1 jl2)
+  | n >= size0 = Empty
+  | n < size1  = dropJ n jl1 +++ jl2
+  | n > 0      = dropJ (n - size1) jl2
+  | otherwise  = jl
+    where
+      size0 = S.getSize . S.size $ m
+      size1 = S.getSize . S.size . tag $ jl1
+dropJ _ _ = Empty
+
+takeJ :: (S.Sized b, Monoid b)
+      => Int -> JoinList b a -> JoinList b a
+takeJ = undefined
+
+az :: JoinList S.Size Char
+az = foldr1 (+++) $ Single (S.Size 1) <$> ['a'..'z']
 
 test1 = do
   print . L.getProduct . tag $ someJoinList
