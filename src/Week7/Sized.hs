4bd2b5fc42648cfac65badf45fcf036e92f339ea
Frank Wang <FrankWangSydney@gmail.com>
2020-02-16 21:05:38 +1100

    Moved Sized type definitions together

--- src/Week7/Sized.hs
+++ src/Week7/Sized.hs
@@ -9,23 +9,19 @@ newtype Size = Size Int
 getSize :: Size -> Int
 getSize (Size i) = i
 
-class Sized a where
-  size :: a -> Size
-
-instance Sized Size where
-  size = id
-
--- This instance means that things like
---   (Foo, Size)
---   (Foo, (Bar, Size))
---   ...
--- are all instances of Sized.
-instance Sized b => Sized (a,b) where
-  size = size . snd
-
 instance Semigroup Size where
   (<>) = (+)
 
 instance Monoid Size where
   mempty  = Size 0
   mappend = (+)
+
+class Sized a where
+  size :: a -> Size
+
+instance Sized Size where
+  size = id
+
+-- The example are (Foo, Size) and (Foo, (Bar, Size))
+instance Sized b => Sized (a,b) where
+  size = size . snd
